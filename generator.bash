7077bb029ed65c932728a27ee968b1cc80674694
Ruby <339808939@qq.com>
2024-01-23 18:56:35 +0800

    Update generator.bash

--- generator.bash
+++ generator.bash
@@ -7,11 +7,10 @@ fi
 
 curl -s https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt -o $WORK_DIR/tmp/all_cn.txt && \
 cat > $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
-/log info "Import cn ipv4 cidr list..."
 /ip firewall address-list remove [/ip firewall address-list find list=cn_ip_cidr]
 /ip firewall address-list
-add address=10.0.0.0/8 list=cn_ip_cidr comment=LanIP
-add address=172.16.0.0/12 list=cn_ip_cidr comment=LanIP
-add address=192.168.0.0/16 list=cn_ip_cidr comment=LanIP
+:do {add address=10.0.0.0/8 list=cn_ip_cidr comment=LanIP} on-error={}
+:do {add address=172.16.0.0/12 list=cn_ip_cidr comment=LanIP} on-error={}
+:do {add address=192.168.0.0/16 list=cn_ip_cidr comment=LanIP} on-error={}
 EOF
 cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc
