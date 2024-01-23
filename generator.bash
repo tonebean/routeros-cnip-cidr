54fb9ee3cd9b1909200bc7cc9b1a5d35c8d72596
Ruby <339808939@qq.com>
2024-01-23 18:52:20 +0800

    Update generator.bash

--- generator.bash
+++ generator.bash
@@ -10,5 +10,8 @@ cat > $WORK_DIR/dist/cn_ip_cidr.rsc << EOF
 /log info "Import cn ipv4 cidr list..."
 /ip firewall address-list remove [/ip firewall address-list find list=cn_ip_cidr]
 /ip firewall address-list
+add address=10.0.0.0/8 list=cn_ip_cidr comment=LanIP
+add address=172.16.0.0/12 list=cn_ip_cidr comment=LanIP
+add address=192.168.0.0/16 list=cn_ip_cidr comment=LanIP
 EOF
 cat $WORK_DIR/tmp/all_cn.txt | awk '{ printf(":do {add address=%s list=cn_ip_cidr} on-error={}\n",$0) }' >> $WORK_DIR/dist/cn_ip_cidr.rsc
